2f20cb42024602c75b744bb2070e706466364840
IrinaSlipchenko <slpchnk.irina@yandex.ru>
2022-04-26 10:05:10 +0300

    Credit Card Mask

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
     public static String maskify(String str) {
+        StringBuilder result = new StringBuilder();
         int n = str.length();
         if (n <= 4){
             return str;
-        }else {
-
+        }else if (n > 4){
+            result.append("#".repeat(n - 4));
+            result.append(str.substring(n - 4, n));
+            return result.toString();
+        }else{
+            throw new UnsupportedOperationException("Unimplemented");
         }
-        throw new UnsupportedOperationException("Unimplemented");
     }
 
 
     public static void main(String[] args) {
-	// Тестируем удаленный репозиторий
+        System.out.println(maskify("4556364607935616"));
     }
 }
