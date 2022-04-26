02e1d178eb8a3c992405ecf7f8cdd005c7cf9553
IrinaSlipchenko <slpchnk.irina@yandex.ru>
2022-04-26 09:37:27 +0300

    Добавила метод maskify

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
+    public static String maskify(String str) {
+        int n = str.length();
+        if (n <= 4){
+            return str;
+        }else {
+
+        }
+        throw new UnsupportedOperationException("Unimplemented");
+    }
+
 
     public static void main(String[] args) {
 	// Тестируем удаленный репозиторий
